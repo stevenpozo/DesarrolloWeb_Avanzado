e4710484dfd180c770802a6139ac985548318368
Steven Jefferson Pozo Analuisa <sjpozo1@espe.edu.ec>
2023-11-17 18:00:09 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,23 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var linkRegistro = document.getElementById('text_registro');
-  linkRegistro.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    $('#myModal').modal('hide');
-
-    setTimeout(function () {
-      $('#myModal1').modal('show');
-    }, 300); // Ajusta el valor del retraso según sea necesario
-  });
-
-  var closeButton = document.querySelector('#myModal1 .close');
-
-  // Agrega un evento de clic al botón "X"
-  closeButton.addEventListener('click', function () {
-    // Recarga la página
-    location.reload();
-  });
-});
 /*cargar htmls*/
 
 function cargarContactos() {
@@ -110,5 +90,67 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   });
+
+
+//FUNCION PARA EL MENU DESPLEGABLE
+(function(){
+  const listElements = document.querySelectorAll('.menu__item--show');
+  const list = document.querySelector('.menu__links');
+  const menu = document.querySelector('.menu__hamburguer');
+
+  const addClick = ()=>{
+      listElements.forEach(element =>{
+          element.addEventListener('click', ()=>{
+
+              
+              let subMenu = element.children[1];
+              let height = 0;
+              element.classList.toggle('menu__item--active');
+
+
+              if(subMenu.clientHeight === 0){
+                  height = subMenu.scrollHeight;
+              }
+
+              subMenu.style.height = `${height}px`;
+
+          });
+      });
+  }
+
+  const deleteStyleHeight = ()=>{
+      listElements.forEach(element=>{
+
+          if(element.children[1].getAttribute('style')){
+              element.children[1].removeAttribute('style');
+              element.classList.remove('menu__item--active');
+          }
+
+      });
+  }
+
+
+  window.addEventListener('resize', ()=>{
+      if(window.innerWidth > 800){
+          deleteStyleHeight();
+          if(list.classList.contains('menu__links--show'))
+              list.classList.remove('menu__links--show');
+
+      }else{
+          addClick();
+      }
+  });
+
+  if(window.innerWidth <= 800){
+      addClick();
+  }
+
+  menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+
+
+
+})();
+
+
 
 
